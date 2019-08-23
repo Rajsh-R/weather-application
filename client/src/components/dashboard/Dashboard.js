d00b6c48f854c1103c43c6faad64e2f7992a1945
RajeshRoy6 <42651960+RajeshRoy6@users.noreply.github.com>
2019-08-24 00:20:14 +0530

    first commit

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+
+import WeatherComponent from "./WeatherComponent";
+import "../../../src/App.css";
+
+class Dashboard extends Component {
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
+  render() {
+    const { user } = this.props.auth;
+
+    return (
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div style={{ marginTop: "4rem" }} className="row">
+          <div className="landing-copy col s12 center-align">
+            <h4>
+              <b>Hey there,</b> {user.name.split(" ")[0]}
+              <p className="flow-text grey-text text-darken-1">
+                Current Weather Forecast
+              </p>
+            </h4>
+            <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem"
+              }}
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Logout
+            </button>
+            <div>
+              <WeatherComponent />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Dashboard);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://user1:user1@cluster0-kicjn.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
